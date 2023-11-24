ef50478ab053ed917edae8dc920743d14f9220a4
Vishesh Dadhich <visheshdadhich20@gmail.com>
2023-11-24 11:40:54 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function userScroll(){
+    const navbar  = document.querySelector('.navbar');
+
+    window.addEventListener('scroll',()=>{
+
+        if(window.scrollY > 49)
+        {
+
+            navbar.classList.add('bg-dark');
+            navbar.classList.add('navbar-sticky');
+        }
+    else
+    {
+
+        navbar.classList.remove('bg-dark');
+        navbar.classList.remove('navbar-sticky');
+    }
+
+    })
+}
+
+window.addEventListener('DOMContentLoaded',userScroll);
